224e706518006bf1acd5960c42da704297a5a145
singhigh <singhigh@users.noreply.github.com>
2016-05-05 16:30:16 +0800

    start project

---
+++ temp.sh
@@ -0,0 +1,26 @@
+apt-get install redis-server screen git curl vim
+wget -c https://storage.googleapis.com/golang/go1.6.2.linux-386.tar.gz
+tar -C /usr/local -xzf go1.6.2.linux-386.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=~/.go
+go get github.com/orvice/shadowsocks-go
+cd ~/.go/src/github.com/orvice/shadowsocks-go/mu
+go get
+go build
+
+cp example.conf ~/.go/bin/config.conf
+cd ~/.go/bin/
+vim ~/.go/bin/config.conf
+
+vim /etc/supervisord.conf
+
+[program:ssserver]
+command = /root/.go/bin/mu
+directory = /root/.go/bin/
+user = root
+autostart = true
+autorestart = true
+
+supervisord -c /etc/supervisord.conf
+
+supervisorctl reload
